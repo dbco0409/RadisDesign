647927b27303fbafa0dc40cb36756282425020de
dbco0409 <prohistory3@gmail.com>
2021-07-30 19:47:38 +0900

    alcon 삭제

--- alcon/js/write.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#file_add").modal();
-});
-
-function file_add(){
-	var n=$("#file_add .cont1 .fileUp").length;
-	$("#file_add .cont1").append('<input type="file" name="fileUp'+n+'"  class="fileUp">');
-}
